0aef888cc8580b687fd89ba975c0acc34888a22b
Komal Ahire <komala18@navgurukul.org>
2023-09-01 00:00:24 +0530

    added window.self

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from "./reportWebVitals";
 
 import "./serviceWorkerRegistration";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,10 @@
 // src/service-worker.js
 
-import { precacheAndRoute } from 'workbox-precaching';
+// import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
 
-precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(window.self.__WB_MANIFEST);
 
 registerRoute(
   ({ url }) => url.origin === 'https://navgurukul.github.io',
