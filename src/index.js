44d7aa6afc12627fd1ac89fe64388f0f41bf125b
Komal Ahire <komala18@navgurukul.org>
2023-08-31 23:16:58 +0530

    added script file in html

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 import "./serviceWorkerRegistration";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
